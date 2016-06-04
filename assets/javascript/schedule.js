1ff6291271311b1a63744468fbecb5c19044d4e0
James <jamesarmandobazan@gmail.com>
2016-06-04 13:44:04 -0500

    finished

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -28,8 +28,13 @@ clickData.on("child_added", function(childSnapshot){
 	var appendRole = childSnapshot.val().roleGivenFB;
 	var appendDate = childSnapshot.val().dateGivenFB;
 	var appendRate = childSnapshot.val().rateGivenFB;
+	var momentMonths = moment(new Date(appendDate));
+	var currentMoment = moment();
+	var howManyMonths = moment().diff(momentMonths, "months");
+	var howMuchPaid = howManyMonths * childSnapshot.val().rateGivenFB;
+	console.log(howManyMonths + " This is the months");
 	console.log(appendName);
-	$('#employeeTable').prepend("<tr><td>" + appendName + "</td><td>" + appendRole + "</td><td>" +  appendDate + "</td><td>" + appendRate + "</td></tr>");
+	$('#employeeTable').prepend("<tr><td>" + appendName + "</td><td>" + appendRole + "</td><td>" +  appendDate + "</td><td>" + howManyMonths + "</td><td>" + appendRate + "</td><td>$" + howMuchPaid + "</td></tr>");
 });
 
 
