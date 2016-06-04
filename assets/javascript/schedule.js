ba035a66a83cf7319fc0f01478755376e19fccc1
James <jamesarmandobazan@gmail.com>
2016-06-04 12:02:20 -0500

    working

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -8,15 +8,31 @@ $(document).on("click", '#addInput', function() {
 	console.log("List of things: " + nameGiven + roleGiven + dateGiven + rateGiven);
 	
 	clickData.push({
-		nameGivenFB: nameGiven,
-		roleGivenFB: roleGiven,
-		dateGivenFB: dateGiven,
-		rateGivenFB: rateGiven
+		"nameGivenFB": nameGiven,
+		"roleGivenFB": roleGiven,
+		"dateGivenFB": dateGiven,
+		"rateGivenFB": rateGiven
 	})
+
+
+
+
+
 
 	return false;
 
 });
 
-clickData.on("value", function(snapshot) {
-	});
+clickData.on("child_added", function(childSnapshot){
+	var appendName = childSnapshot.val().nameGivenFB;
+	var appendRole = childSnapshot.val().roleGivenFB;
+	var appendDate = childSnapshot.val().dateGivenFB;
+	var appendRate = childSnapshot.val().rateGivenFB;
+	console.log(appendName);
+	$('#employeeTable').prepend("<tr><td>" + appendName + "</td><td>" + appendRole + "</td><td>" +  appendDate + "</td><td>" + appendRate + "</td></tr>");
+});
+
+
+///dateAdded: Firebase.ServerValue.TIMESTAMP
+//.on('child_added', function() {});
+
